1b33f642f8fe217ce4f18e50e3166c19c742c8ce
jbrilha <j.brilha@campus.fct.unl.pt>
2024-08-12 03:59:00 +0100

    feat: 404 Route handling

--- handlers/routes.go
+++ handlers/routes.go
@@ -1,19 +1,11 @@
 package handlers
 
 import (
+	"goethe/views/routes"
+
 	"github.com/labstack/echo/v4"
 )
 
-func SetRoutes(e *echo.Echo) {
-	e.Static("/public", "public")
-
-	e.GET("/", HandleHome)
-
-	e.GET("/bookshelf", BookshelfBase)
-	e.POST("/bookshelf/add-book", AddBook)
-	e.DELETE("/bookshelf/remove-book", RemoveBook)
-	e.GET("/bookshelf/book", HandleBook)
-
-	e.GET("/blog", BlogBase)
-	e.GET("/blog/post", BlogPost)
+func Route404(c echo.Context) error {
+	return Render(c, routes.Route404())
 }
