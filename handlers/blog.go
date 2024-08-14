1a8c1f2054654561f5a5c468beaaaf6bdf14bf90
jbrilha <j.brilha@campus.fct.unl.pt>
2024-08-15 00:41:40 +0100

    chore: move db access away from template

--- handlers/blog.go
+++ handlers/blog.go
@@ -2,26 +2,49 @@ package handlers
 
 import (
 	"fmt"
-	"goethe/data"
 	"goethe/db"
 	"goethe/views/blog"
+	"goethe/views/routes"
+	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 )
 
 func BlogBase(c echo.Context) error {
-	return Render(c, blog.Index(data.GetPosts()))
+	return Render(c, blog.Index(db.GetBlogPosts()))
 }
 
 func BlogPost(c echo.Context) error {
-	id, err := strconv.Atoi(c.Param("id"))
-    if err != nil {
-		fmt.Println("Invalid query param")
-    }
+	p := c.Param("id")
 
-    fmt.Println(id)
-    post := db.GetBlogPost(id)
+	if idStr := strings.TrimSuffix(p, ".json"); idStr != p {
+		id, err := strconv.Atoi(idStr)
+
+		if err != nil {
+			fmt.Println("Invalid param:", idStr)
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid blog post ID"})
+		}
+
+		post, err := db.GetBlogPost(id)
+		if err != nil {
+			return c.JSON(http.StatusNotFound, map[string]string{"error": "Blog post not found"})
+		}
+
+		return c.JSON(http.StatusOK, post)
+	}
+
+	id, err := strconv.Atoi(p)
+	if err != nil {
+		fmt.Println("Invalid param")
+	}
+
+	post, err := db.GetBlogPost(id)
+	if err != nil {
+		fmt.Println(err)
+		return Render(c, routes.Route404())
+	}
 
 	return Render(c, blog.Post(post))
 }
