f95e5565fc7a10629e99e4c0f0adcdbcbfd91a4e
jbrilha <j.brilha@campus.fct.unl.pt>
2024-08-14 19:31:39 +0100

    feat: JWT registered claims

--- auth/auth.go
+++ auth/auth.go
@@ -5,24 +5,13 @@ import (
 	"net/http"
 	"time"
 
+	"goethe/data"
 	"goethe/env"
 	"goethe/util"
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 )
-
-func CreateJWT(str string) (string, error) {
-	claims := jwt.MapClaims{
-		"foo": "bar",
-		"nbf": time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	secret := env.JWTSecret()
-
-	return token.SignedString([]byte(secret))
-}
 
 // Checks for JWT validity, if OK returns the fn handlerFunc; otherwise returns the altfn
 // TODO this can probably be improved but I'm not sure how to do error handling with HTMX yet
@@ -38,7 +27,7 @@ func WithJWT(fn echo.HandlerFunc, altfn echo.HandlerFunc) echo.HandlerFunc {
 			}
 			return c.JSON(http.StatusUnauthorized, map[string]string{"error": "Unauthorized"})
 		} else {
-			token, err := ValidateJWT(cookie.Value)
+			token, err := validateJWT(cookie.Value)
 			if err != nil {
 				fmt.Println(err)
 				if c.Request().Header.Get("HX-Request") != "" {
@@ -50,42 +39,32 @@ func WithJWT(fn echo.HandlerFunc, altfn echo.HandlerFunc) echo.HandlerFunc {
 			}
 
 			claims := token.Claims.(jwt.MapClaims)
-			//map[foo:bar nbf:1.4444784e+09] example
 
-			fmt.Println(claims)
+            fmt.Println("CLAIMS:", claims)
 
 		}
 		return fn(c)
 	}
 }
 
-func CheckForJWT(fn echo.HandlerFunc, altfn echo.HandlerFunc) echo.MiddlewareFunc {
-	return func(hf echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			cook, err := util.ReadCookie(c, "JWT")
-			if err != nil {
-				fmt.Println("err jwt middleware", err)
-				if c.Request().Header.Get("HX-Request") != "" {
-					return altfn(c)
-				}
-				return c.JSON(http.StatusUnauthorized, map[string]string{"error": "Unauthorized"})
-			} else {
-				_, err = ValidateJWT(cook.Value)
-				if err != nil {
-					fmt.Println(err)
-					if c.Request().Header.Get("HX-Request") != "" {
-						return altfn(c)
-					}
-					return c.JSON(http.StatusUnauthorized, map[string]string{"error": "Unauthorized"})
-				}
-
-			}
-			return hf(c)
-		}
+func CreateJWT(u data.User) (string, error) {
+	claims := jwt.RegisteredClaims{
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		NotBefore: jwt.NewNumericDate(time.Now()),
+		Issuer:    "goethe",
+		Subject:   u.Username,
+		ID:        string(u.ID),
+		// Audience:  []string{"somebody_else"},
 	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	secret := env.JWTSecret()
+
+	return token.SignedString([]byte(secret))
 }
 
-func ValidateJWT(tokenString string) (*jwt.Token, error) {
+func validateJWT(tokenString string) (*jwt.Token, error) {
 	secret := env.JWTSecret()
 
 	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
@@ -95,5 +74,34 @@ func ValidateJWT(tokenString string) (*jwt.Token, error) {
 
 		return []byte(secret), nil
 	})
-
 }
+
+func CheckPassword(encryptedPassword, password string) bool {
+    return encryptedPassword == password
+}
+
+// func CheckForJWT(fn echo.HandlerFunc, altfn echo.HandlerFunc) echo.MiddlewareFunc {
+// 	return func(hf echo.HandlerFunc) echo.HandlerFunc {
+// 		return func(c echo.Context) error {
+// 			cook, err := util.ReadCookie(c, "JWT")
+// 			if err != nil {
+// 				fmt.Println("err jwt middleware", err)
+// 				if c.Request().Header.Get("HX-Request") != "" {
+// 					return altfn(c)
+// 				}
+// 				return c.JSON(http.StatusUnauthorized, map[string]string{"error": "Unauthorized"})
+// 			} else {
+// 				_, err = ValidateJWT(cook.Value)
+// 				if err != nil {
+// 					fmt.Println(err)
+// 					if c.Request().Header.Get("HX-Request") != "" {
+// 						return altfn(c)
+// 					}
+// 					return c.JSON(http.StatusUnauthorized, map[string]string{"error": "Unauthorized"})
+// 				}
+//
+// 			}
+// 			return hf(c)
+// 		}
+// 	}
+// }
