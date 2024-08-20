390b52b6c3ae7a5623f57caffbe72e7d624c448f
jbrilha <j.brilha@campus.fct.unl.pt>
2024-08-20 01:15:22 +0100

    feat: book struct

--- data/book.go
+++ data/book.go
@@ -1,14 +1,22 @@
 package data
 
 type Book struct {
-	Title  string
-	Author string
+	ID          int      `json:"id"`
+	ISBN10      string   `json:"isbn10"`
+	ISBN13      string   `json:"isbn13"`
+	Title       string   `json:"title"`
+	Authors     []string `json:"authors"`
+	Publishers  []string `json:"publishers"`
+	Pages       int      `json:"pages"`
+	PublishDate string   `json:"publish_date"`
+	Description string   `json:"description"`
+	Languages   []string `json:"languages"`
 }
 
 func GetBooks() []Book {
 	return []Book{
-		{Title: "Book of Disquiet", Author: "Fernando Pessoa"},
-		{Title: "1984", Author: "George Orwell"},
-		{Title: "My Life Had Stood a Loaded Gun", Author: "Emily Dickinson"},
+		{Title: "Book of Disquiet", Authors: []string{"Fernando Pessoa"}},
+		{Title: "1984", Authors: []string{"George Orwell"}},
+		{Title: "My Life Had Stood a Loaded Gun", Authors: []string{"Emily Dickinson"}},
 	}
 }
