60360e62cb66a3d5c4b19a0e010190bce095210d
jbrilha <j.brilha@campus.fct.unl.pt>
2024-08-20 01:19:21 +0100

    feat: db interaction for books

--- handlers/bookshelf.go
+++ handlers/bookshelf.go
@@ -1,38 +1,53 @@
 package handlers
 
 import (
+	"fmt"
 	"goethe/data"
+	"goethe/db"
 	"goethe/views/bookshelf"
 
 	"github.com/labstack/echo/v4"
 )
 
 func AddBook(c echo.Context) error {
-	title := c.FormValue("title")
-	author := c.FormValue("author")
+	// title := c.FormValue("title")
+	// author := c.FormValue("author")
 
-	book := data.Book{Title: title, Author: author}
+	b := data.Book{
+		ISBN13:  "9780241251409",
+        Title: "My Life Had Stood A Loaded Gun",
+        Authors: []string{"Emily Dickinson"},
+        Publishers: []string{"Penguin"},
+        Pages: 52,
+        PublishDate: "2016",
+        Languages: []string{"English"},
+	}
+	_, err := db.InsertBook(&b)
+	if err != nil {
+		fmt.Println("err in insetion:", err)
+	}
+	fmt.Println(b.ID)
 
-	return Render(c, bookshelf.AddBook(book))
+	return Render(c, bookshelf.AddBook(b))
 }
 
 func RemoveBook(c echo.Context) error {
 	title := c.FormValue("title")
 	author := c.FormValue("author")
 
-	book := data.Book{Title: title, Author: author}
+	book := data.Book{Title: title, Authors: []string{author}}
 
 	return Render(c, bookshelf.RemoveBook(book))
 }
 
 func HandleBook(c echo.Context) error {
-    book := data.Book {
-        Title: "AA",
-        Author: "BB",
-    }
-    return Render(c, bookshelf.Show(book))
+	book := data.Book{
+		Title:  "AA",
+		Authors: []string{"BB", "CC"},
+	}
+	return Render(c, bookshelf.Show(book))
 }
 
 func BookshelfBase(c echo.Context) error {
-    return Render(c, bookshelf.Index(data.GetBooks()))
+	return Render(c, bookshelf.Index(db.GetBooks()))
 }
