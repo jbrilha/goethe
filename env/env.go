e8c7c677c1384ed2fb8f8e38592f50dfa75aa41f
jbrilha <j.brilha@campus.fct.unl.pt>
2024-08-29 02:48:08 +0100

    feat: add more env variable access

--- env/env.go
+++ env/env.go
@@ -19,10 +19,27 @@ func Port() string {
 	return os.Getenv("PORT")
 }
 
+func JWTSecret() string {
+	return os.Getenv("JWT_SECRET")
+}
+
+func DBUser() string {
+	return os.Getenv("PG_USER")
+}
+
+func DBPassword() string {
+	return os.Getenv("PG_PASSWORD")
+}
+
+func DBName() string {
+	return os.Getenv("PG_DB")
+}
+
+func DBHost() string {
+	return os.Getenv("PG_HOST")
+}
+
 func DBConn() string {
 	return os.Getenv("PG_CONN")
 }
 
-func JWTSecret() string {
-	return os.Getenv("JWT_SECRET")
-}
